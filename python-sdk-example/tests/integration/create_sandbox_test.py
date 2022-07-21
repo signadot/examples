7c0aeacd96147729ff3d45abcc9649ac41392c28
jmsktm <james.singh.np@gmail.com>
2022-07-21 16:30:33 -0700

    Update readiness check in Python SDK example

--- python-sdk-example/tests/integration/create_sandbox_test.py
+++ python-sdk-example/tests/integration/create_sandbox_test.py
@@ -82,20 +82,10 @@ class TestBasic(unittest.TestCase):
             raise RuntimeError("Endpoint `hotrod-route` missing")
         cls.endpoint_url = filtered_endpoints[0].url
 
-        sandbox_ready = False
-        max_attempts = 20
-        print("Checking sandbox readiness")
-        for i in range(1, max_attempts):
-            print("Attempt: {}/{}".format(i, max_attempts))
-            if sandbox.status.ready:
-                sandbox_ready = True
-                print("Sandbox is ready!")
-                break
+        # Code block to wait until sandbox is ready
+        while not sandbox.status.ready:
             time.sleep(5)
             sandbox = cls.sandboxes_api.get_sandbox(cls.SIGNADOT_ORG, cls.sandbox_name)
-
-        if not sandbox_ready:
-            raise RuntimeError("Sandbox didn't get to Ready state")
 
     def test_valid_result(self):
         pickup = "123"
